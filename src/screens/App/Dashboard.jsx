3f726ee37ffdcff1b5dc475838f14f1caeb6b38e
shanbora <chanbora@logilink.fr>
2024-02-08 15:09:02 +0100

    tmp

---
+++ src/screens/App/Dashboard.jsx
@@ -0,0 +1,31 @@
+// import React from "react";
+//
+// import { View, Text } from "react-native";
+// import { getGenericPassword } from "react-native-keychain";
+//
+// export function Dashboard ()
+// {
+//     getData('token').then(r => console.log(r))
+//
+//     return (
+//         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//             <Text>Details Screen</Text>
+//         </View>
+//     );
+// }
+//
+// const getData = async (key) => {
+//     try {
+//         const value = await getGenericPassword();
+//         if (value !== null) {
+//             console.log('Données récupérées avec succès :', value);
+//             return value;
+//         } else {
+//             console.log('Aucune donnée trouvée pour la clé :', key);
+//             return null;
+//         }
+//     } catch (error) {
+//         console.error('Erreur lors de la récupération des données :', error);
+//         return null;
+//     }
+// };
---
+++ src/screens/Security/LoginScreen.jsx
@@ -0,0 +1,95 @@
+// import React, { useState } from 'react';
+//
+// import { API_URL } from '@env';
+//
+// import axios from 'axios';
+//
+// import { View, TextInput, StyleSheet, Text, TouchableOpacity } from 'react-native';
+// import { setGenericPassword } from "react-native-keychain";
+//
+// export const LoginScreen = ({ navigation }) => {
+//     const [credentials, setCredentials] = useState({
+//         username: '',
+//         password: '',
+//     });
+//     console.log(API_URL + '/login_check')
+//
+//     const handleLogin = async () => {
+//         try {
+//             // Envoie les informations de connexion sous forme de JSON
+//             const response = await axios.post(
+//                 API_URL + '/login_check',
+//                 credentials,
+//                 {
+//                     headers: {'Content-Type': 'application/json'}
+//                 }
+//             );
+//
+//             await setGenericPassword("token", response.data.token);
+//             console.log('Token', response.data);
+//
+//             navigation.navigate('Dashboard')
+//         } catch (error) {
+//             console.log('Erreur de LoginService', error);
+//         }
+//     };
+//
+//     return (
+//         <View style={styles.container}>
+//             <Text style={styles.title}>Connexion</Text>
+//             <TextInput
+//                 style={styles.input}
+//                 placeholder="Nom utilisateur"
+//                 keyboardType="email-address"
+//                 autoCapitalize="none"
+//                 value={credentials.username}
+//                 onChangeText={(text) => setCredentials({ ...credentials, username: text })}
+//             />
+//
+//             <TextInput
+//                 style={styles.input}
+//                 placeholder="Mot de passe"
+//                 autoCapitalize="none"
+//                 secureTextEntry
+//                 value={credentials.password}
+//                 onChangeText={(text) => setCredentials({ ...credentials, password: text })}
+//             />
+//             <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+//                 <Text style={styles.buttonText}>Se connecter</Text>
+//             </TouchableOpacity>
+//         </View>
+//     );
+// };
+//
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         padding: 16,
+//     },
+//     title: {
+//         fontSize: 24,
+//         marginBottom: 16,
+//     },
+//     input: {
+//         width: '100%',
+//         height: 40,
+//         borderColor: 'gray',
+//         backgroundColor: 'white',
+//         borderWidth: 1,
+//         borderRadius: 8,
+//         marginBottom: 16,
+//         paddingLeft: 8,
+//     },
+//     loginButton: {
+//         backgroundColor: 'blue',
+//         padding: 10,
+//         borderRadius: 8,
+//         alignItems: 'center',
+//     },
+//     buttonText: {
+//         color: 'white',
+//         fontSize: 16,
+//     },
+// });
