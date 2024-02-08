5f0e18f152ef9d56b7fb37aa72522ab0e96d112c
shanbora <chanbora@logilink.fr>
2024-02-08 15:18:58 +0100

    instal navigation

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+          <Stack.Navigator initialRouteName="Home">
+              <Stack.Screen
+                  name="Home"
+                  component={HomeScreen}
+                  options={{ title: 'Logilink' }}
+              />
+              <Stack.Screen name="Dashboard" component={Dashboard} />
+          </Stack.Navigator>
+        <StatusBar style="auto" />
+      </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function HomeScreen({ navigation }) {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Home Screen</Text>
+            <Button
+                title="Go to Details"
+                onPress={() => navigation.navigate('Dashboard')}
+            />
+        </View>
+    );
+}
+
+
+function Dashboard() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Dashboard</Text>
+        </View>
+    );
+}
